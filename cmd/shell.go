385bc6b89b775cc09a3a6f2207bd9b429e1a1175
claudenm <claude.mendelson@gmail.com>
2019-05-12 12:13:12 -0400

    Add a new command that prints a sourceable configuration to STDOUT.

---
+++ cmd/shell.go
@@ -0,0 +1,112 @@
+package cmd
+
+import (
+	"fmt"
+
+	"os"
+	"time"
+
+	"github.com/99designs/keyring"
+	analytics "github.com/segmentio/analytics-go"
+	"github.com/segmentio/aws-okta/lib"
+	"github.com/spf13/cobra"
+)
+
+// shellCmd represents the shell command
+var shellCmd = &cobra.Command{
+	Use:       "shell <profile>",
+	Short:     "shell writes a sourceable export statements to set environment variables for the current shell for the specified profile",
+	RunE:      shellRun,
+	Example:   "source <$(aws-okta shell test)",
+	ValidArgs: listProfileNames(mustListProfiles()),
+}
+
+func init() {
+	RootCmd.AddCommand(shellCmd)
+	shellCmd.Flags().DurationVarP(&sessionTTL, "session-ttl", "t", time.Hour, "Expiration time for okta role session")
+	shellCmd.Flags().DurationVarP(&assumeRoleTTL, "assume-role-ttl", "a", time.Hour, "Expiration time for assumed role")
+}
+
+func shellRun(cmd *cobra.Command, args []string) error {
+	if len(args) < 1 {
+		return ErrTooFewArguments
+	}
+
+	profile := args[0]
+	config, err := lib.NewConfigFromEnv()
+	if err != nil {
+		return err
+	}
+
+	profiles, err := config.Parse()
+	if err != nil {
+		return err
+	}
+
+	if _, ok := profiles[profile]; !ok {
+		return fmt.Errorf("Profile '%s' not found in your aws config. Use list command to see configured profiles", profile)
+	}
+
+	updateMfaConfig(cmd, profiles, profile, &mfaConfig)
+
+	// check for an assume_role_ttl in the profile if we don't have a more explicit one
+	if !cmd.Flags().Lookup("assume-role-ttl").Changed {
+		if err := updateDurationFromConfigProfile(profiles, profile, &assumeRoleTTL); err != nil {
+			fmt.Fprintln(os.Stderr, "warning: could not parse duration from profile config")
+		}
+	}
+
+	opts := lib.ProviderOptions{
+		MFAConfig:          mfaConfig,
+		Profiles:           profiles,
+		SessionDuration:    sessionTTL,
+		AssumeRoleDuration: assumeRoleTTL,
+	}
+
+	var allowedBackends []keyring.BackendType
+	if backend != "" {
+		allowedBackends = append(allowedBackends, keyring.BackendType(backend))
+	}
+
+	kr, err := lib.OpenKeyring(allowedBackends)
+	if err != nil {
+		return err
+	}
+
+	if analyticsEnabled && analyticsClient != nil {
+		analyticsClient.Enqueue(analytics.Track{
+			UserId: username,
+			Event:  "Ran Command",
+			Properties: analytics.NewProperties().
+				Set("backend", backend).
+				Set("aws-okta-version", version).
+				Set("profile", profile).
+				Set("command", "shell"),
+		})
+	}
+
+	p, err := lib.NewProvider(kr, profile, opts)
+	if err != nil {
+		return err
+	}
+
+	creds, err := p.Retrieve()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("export AWS_ACCESS_KEY_ID=%s\n", creds.AccessKeyID)
+	fmt.Printf("export AWS_SECRET_ACCESS_KEY=%s\n", creds.SecretAccessKey)
+	fmt.Printf("export AWS_OKTA_PROFILE=%s\n", profile)
+
+	if region, ok := profiles[profile]["region"]; ok {
+		fmt.Printf("export AWS_DEFAULT_REGION=%s\n", region)
+		fmt.Printf("export AWS_REGION=%s\n", region)
+		os.Setenv("AWS_REGION", region)
+	}
+	if creds.SessionToken != "" {
+		fmt.Printf("export AWS_SESSION_TOKEN=%s\n", creds.SessionToken)
+		fmt.Printf("export AWS_SECURITY_TOKEN=%s\n", creds.SessionToken)
+	}
+
+	return nil
+}
