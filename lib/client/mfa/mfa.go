fc389db98007a997e0362e5d3f5e6cee9b5c96f4
Josh Switnicki <switj@autonomic.ai>
2019-11-14 14:40:22 -0500

    remove pointer to interface

--- lib/client/mfa/mfa.go
+++ lib/client/mfa/mfa.go
@@ -12,11 +12,11 @@ type Input interface {
 }
 
 type Config struct {
-	Provider   string  // Which MFA provider to use when presented with an MFA challenge
-	FactorType string  // Which of the factor types of the MFA provider to use
-	DuoDevice  string  // Which DUO device to use for DUO MFA
-	Device     *Device // The implementation that interacts with the device
-	Id         string  // the unique id for the MFA device provided by Okta
+	Provider   string // Which MFA provider to use when presented with an MFA challenge
+	FactorType string // Which of the factor types of the MFA provider to use
+	DuoDevice  string // Which DUO device to use for DUO MFA
+	Device     Device // The implementation that interacts with the device
+	Id         string // the unique id for the MFA device provided by Okta
 }
 
 type Device interface {
